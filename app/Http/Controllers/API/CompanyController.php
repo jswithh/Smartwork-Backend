f2b9cce5ca56011482e0ca7674ef996ea4d3c6c6
hariapermana <jswithhari@gmail.com>
2023-02-07 15:18:45 +0700

    push company logo

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -57,7 +57,7 @@ class CompanyController extends Controller
     public function create(CreateCompanyRequest $request){
         try {
             if($request->file('logo')){
-                $path = $request->file('logo')->store('public/company_logo');
+                 $path = url().'storage/'.$request->file('logo')->store('public/company_logo');
             }
             $company = Company::create([
                 'name' => $request->name,
