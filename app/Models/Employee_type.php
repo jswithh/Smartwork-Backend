73709b42be64aaadf3a7bc5a40c53c231a47c453
hariapermana <jswithhari@gmail.com>
2023-03-08 11:39:18 +0700

    fix employee type
    fix employee type

--- app/Models/Employee_type.php
+++ app/Models/Employee_type.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Employee_type extends Model
+class Employee_Type extends Model
 {
     use HasFactory, SoftDeletes;
 
