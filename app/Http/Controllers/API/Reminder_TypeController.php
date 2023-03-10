a6854bac7b4eca5e8f56fd26226f90b92656b6d2
hariapermana <jswithhari@gmail.com>
2023-03-10 09:41:39 +0700

    fix error

--- app/Http/Controllers/API/Reminder_TypeController.php
+++ app/Http/Controllers/API/Reminder_TypeController.php
@@ -3,84 +3,88 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-Use App\Models\Reminder_type;
+use App\Models\Reminder_Type;
 use App\Helpers\ResponseFormatter;
 
 use Illuminate\Http\Request;
 use Vinkla\Hashids\Facades\Hashids;
-class Reminder_typeController extends Controller
+
+class Reminder_TypeController extends Controller
 {
-    public function create(Request $request){
+    public function create(Request $request)
+    {
 
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
         ]);
 
-        $reminder_type = Reminder_type::create($request->all());
-       
+        $Reminder_Type = Reminder_Type::create($request->all());
 
-        if($reminder_type){
-            return ResponseFormatter::success($reminder_type, 'Reminder type Created');
+
+        if ($Reminder_Type) {
+            return ResponseFormatter::success($Reminder_Type, 'Reminder type Created');
         }
 
         return ResponseFormatter::error(null, 'Reminder type Failed to Create');
-}
+    }
 
-    public function fetch(Request $request){
+    public function fetch(Request $request)
+    {
         $id = $request->input('id');
         $limit = $request->input('limit', 10);
 
         // get multiple data
-        $reminder_typeQuery = Reminder_type::query();
+        $Reminder_TypeQuery = Reminder_Type::query();
 
         // get single data
 
-        if($request->has('id')){
-            $id = Hashids::decode($id); 
-            $reminder_type= $reminder_typeQuery->where('id', $id)->get();
+        if ($request->has('id')) {
+            $id = Hashids::decode($id);
+            $Reminder_Type = $Reminder_TypeQuery->where('id', $id)->get();
 
-            if($reminder_type->isNotEmpty()){
-                return ResponseFormatter::success($reminder_type, 'Reminder type Found');
+            if ($Reminder_Type->isNotEmpty()) {
+                return ResponseFormatter::success($Reminder_Type, 'Reminder type Found');
             }
             return ResponseFormatter::error('Reminder type Not Found', 404);
         }
-        $reminder_type = $reminder_typeQuery->paginate($limit);
-        if($reminder_type->isNotEmpty()){
-            return ResponseFormatter::success($reminder_type, 'Reminder type Found');
+        $Reminder_Type = $Reminder_TypeQuery->paginate($limit);
+        if ($Reminder_Type->isNotEmpty()) {
+            return ResponseFormatter::success($Reminder_Type, 'Reminder type Found');
         }
         return ResponseFormatter::error('Reminder type Not Found', 404);
-
-        
     }
 
-    public function update(Request $request, $id){
+    public function update(Request $request, $id)
+    {
 
         $request->validate([
             'name' => ['nullable', 'string', 'max:255'],
         ]);
         $id = Hashids::decode($id)[0];
-        $reminder_type = Reminder_type::find($id);
+        $Reminder_Type = Reminder_Type::find($id);
 
-        if($reminder_type){
-            $reminder_type->update($request->all()
+        if ($Reminder_Type) {
+            $Reminder_Type->update(
+                $request->all()
             );
 
-            return ResponseFormatter::success($reminder_type, 'Reminder type Updated');
+            return ResponseFormatter::success($Reminder_Type, 'Reminder type Updated');
         }
 
         return ResponseFormatter::error(null, 'Reminder type Failed to Update');
     }
 
-    public function delete($id){
+    public function delete($id)
+    {
         $id = Hashids::decode($id)[0];
-        $reminder_type = Reminder_type::find($id);
+        $Reminder_Type = Reminder_Type::find($id);
 
-        if($reminder_type){
-            $reminder_type->delete();
+        if ($Reminder_Type) {
+            $Reminder_Type->delete();
 
-            return ResponseFormatter::success($reminder_type, 'Reminder type Deleted');
+            return ResponseFormatter::success($Reminder_Type, 'Reminder type Deleted');
         }
 
-        return ResponseFormatter::error('Reminder type Failed to Delete',404);
+        return ResponseFormatter::error('Reminder type Failed to Delete', 404);
     }
 }
